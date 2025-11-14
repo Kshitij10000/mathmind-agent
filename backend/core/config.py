881133d1d563f02f8031acf866cffe7666f63430
Kshitij Sarve <kshitijsarve10000@gmail.com>
2025-11-14 16:16:27 +0530

    Enhance backend configuration by extending the Settings class with additional API keys and parameters. Introduce a new MCP server module for web search functionality using Tavily API, including error handling and tool management.

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,14 @@
+# core/config.py
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings():
+class Settings(BaseSettings):
     
     GEMINI_API_KEY: str
-    
-    model_config = SettingsConfigDict(env_file=".env")
+    QDRANT_COLLECTION_NAME: str
+    QDRANT_API_KEY: str
+    QDRANT_URL: str
+    VECTOR_DIMENSIONS: int = 768
+    TAVILY_API: str
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", case_sensitive=True, extra="ignore")
+
+settings = Settings()
---
+++ backend/math_agent/llm/mcp_servers.py
@@ -0,0 +1,35 @@
+# backend\math_agent\llm\mcp_servers.py
+from langchain_mcp_adapters.client import MultiServerMCPClient  
+from langchain_mcp_adapters.tools import load_mcp_tools
+
+from core.config import settings
+
+async def search_web_mcp(question: str) -> str:
+    api_key = settings.TAVILY_API
+    url = f'https://mcp.tavily.com/mcp/?tavilyApiKey={api_key}'
+
+    client = MultiServerMCPClient(
+        {
+            "tavily_search":{
+                "transport": "streamable_http",
+                "url": url
+            }
+        }
+    )
+
+    try: 
+        tools = await client.get_tools()
+        if not tools:
+            return "Error: No tools found on MCP server."
+        search_tool = tools[0]
+        print(f"[Web Search] Found MCP tool: {search_tool.name}")
+
+        # 4. Call the tool
+        # The Tavily tool takes a 'query' argument
+        result_str = await search_tool.ainvoke({"query": question})
+        print("[Web Search] Received context from MCP.")
+        return str(result_str)
+
+    except Exception as e:
+        print(f"[Web Search] MCP Error: {e}")
+        return f"Web search (MCP) failed: {e}"
